1257f4cc9a1327aabd51b998b79d3000787f64a4
rob <r.m.rimmer@2017.ljmu.ac.uk>
2020-10-23 21:17:07 +0100

    hey

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>everton will win the league yipeeeeee!!!!</Text>
+      <Text>Rob!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
