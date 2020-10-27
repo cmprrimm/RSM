941b00e7ccfa2bdd652282c6993cc3f7a4bea409
rob <r.m.rimmer@2017.ljmu.ac.uk>
2020-10-27 22:09:38 +0000

    adding textinput boxes and spacing out items.

--- App.tsx
+++ App.tsx
@@ -1,28 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, SafeAreaView, Linking } from 'react-native';
 
+const mainApp = () => {
+  const [value, onChangeText] = React.useState();
+  const [value1, onChangeText1] = React.useState();
 
-export default function App()  {
     return (
       <SafeAreaView style={styles.container}>
        <StatusBar style="light" />
 
+
+        <View style={styles.title}>
+        <Text style={{color: '#fff', fontSize: 32}}>Patient Access!</Text>
+        </View>
+
+
+        <View style={{width: 200, height: 30, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center', marginTop: 70}}>
+        <TextInput
+               onChangeText={text => onChangeText(text)}
+               value={value}
+             />
+        </View>
+
+
+        <View style={{width: 200, height: 30, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center', marginTop: 40}}>
+        <TextInput
+               onChangeText1={text1 => onChangeText1(text1)}
+               value1={value1}
+             />
+         </View>
+
+
+        <View style={styles.login}>
         <Button
         onPress={() => {
         alert('Everton will win the league!');
         }}
         title="Log In"
-        color="#000000"
+        color="#008000"
         accessibilityLabel="Click here to Log In"/>
 
+         <Text style={{color: 'blue', marginTop: 20}}
+            onPress={() => Linking.openURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO')}>
+              Click to Reset Password
+         </Text>
+        </View>
+
+
+        <View style={styles.signUp}>
         <Button
         onPress={() => {
         alert('Everton 2021 league champions!');
         }}
         title="Sign Up"
-        color="#000000"
+        color="#FF0000"
         accessibilityLabel="Click here to Sign Up"/>
+        </View>
 
       </SafeAreaView>
     );
@@ -37,5 +71,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
-}
+},
+  title: {
+     flex: 2,
+     flexDirection: 'column',
+     color: '#888',
+     backgroundColor: '#000000',
+     justifyContent: 'flex-end',
+},
+login: {
+     flex: 3,
+     flexDirection: 'column',
+     justifyContent: 'center',
+     alignItems: 'center',
+},
+signUp: {
+    flex: 3,
+    flexDirection: 'column',
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
 });
+
+export default mainApp;
